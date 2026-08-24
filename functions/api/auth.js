27731a5bc02bb1171dee401398b3d60bc02183e1
sargeapp <sargeappdevelopments@gmail.com>
2026-08-24 20:18:20 +0100

    delation

--- functions/api/auth.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-  const { env } = context;
-  const client_id = env.GITHUB_CLIENT_ID;
-  const redirect_uri = "https://flying.sarge-smiffy.com/api/callback";
-  
-  const authUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=${encodeURIComponent('repo,user')}`;
-  
-  return Response.redirect(authUrl, 302);
-}
